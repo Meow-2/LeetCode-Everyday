b8f6b028a515d02e73f76b5d525c93012cbf6b6a
N.O.Atomusk <orzkey@outlook.com>
2022-02-19 15:46:11 +0800

    :tada:2022 Day 5,change to slide windows

--- hash-table/438-找到字符串中所有字母异位词.cpp
+++ hash-table/438-找到字符串中所有字母异位词.cpp
@@ -49,17 +49,22 @@ public:
 
     vector<int> findAnagrams(string s, string p)
     {
-        vector<int> goal(26), cur(26), res;
-        for (char c : p)
-            goal[c - 'a']++;
-        for (int i = 0; i < s.size(); i++) {
-            cur[s[i] - 'a']++;
-            if (i >= p.size())
-                cur[s[i - p.size()] - 'a']--;
-            if (cur == goal)
-                res.push_back(i - p.size() + 1);
+        vector<int> goal(26, 0), cur(26, 0), index;
+        for (auto i : p) {
+            goal[i - 'a']++;
         }
-        return res;
+        int left = 0, right = 0;
+        while (right < s.length()) {
+            cur[s[right] - 'a']++;
+            right++;
+            if (cur == goal)
+                index.push_back(left);
+            while (right - left >= p.length()) {
+                cur[s[left] - 'a']--;
+                left++;
+            }
+        }
+        return index;
     }
 };
 // @lc code=end
