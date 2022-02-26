dc88166c45b0003a83c25c693e8a53806ea07157
N.O.Atomusk <orzkey@outlook.com>
2022-02-26 18:48:16 +0800

    :tada:use hashmap to save distance every circle

--- hash-table/447-回旋镖的数量.cpp
+++ hash-table/447-回旋镖的数量.cpp
@@ -4,12 +4,29 @@
  * [447] 回旋镖的数量
  */
 
+#include <cmath>
+#include <unordered_map>
+#include <vector>
+using namespace std;
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    int numberOfBoomerangs(vector<vector<int>>& points) {
-
+    int numberOfBoomerangs(vector<vector<int>>& points)
+    {
+        int                     result = 0;
+        unordered_map<int, int> distanceCount;
+        for (auto& i : points) {
+            distanceCount.clear();
+            for (auto& j : points) {
+                if (i != j) {
+                    int temp = pow(i[0] - j[0], 2) + pow(i[1] - j[1], 2);
+                    result += 2 * distanceCount[temp];
+                    distanceCount[temp]++;
+                }
+            }
+        }
+        return result;
     }
 };
 // @lc code=end
-
