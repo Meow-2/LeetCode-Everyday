931ba2ce1b20e3cc8035c940135fdf246a48a8a9
N.O.Atomusk <orzkey@outlook.com>
2022-02-26 23:57:25 +0800

    :tada:hashmap

--- hash-table/219-存在重复元素 II.cpp
+++ hash-table/219-存在重复元素 II.cpp
@@ -5,10 +5,23 @@
  */
 
 // @lc code=start
+#include <unordered_map>
+#include <vector>
+using namespace std;
 class Solution
 {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k)
-    {}
+    {
+        unordered_map<int, int> numIndex;
+        for (int i = 0; i < nums.size(); i++) {
+            if (numIndex.find(nums[i]) != numIndex.end()) {
+                if (i - numIndex[nums[i]] <= k)
+                    return true;
+            }
+            numIndex[nums[i]] = i;
+        }
+        return false;
+    }
 };
 // @lc code=end
