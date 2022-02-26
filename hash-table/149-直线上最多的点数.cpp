022e957e9bbc95b796973f71c6b6588d0848c6ae
N.O.Atomusk <orzkey@outlook.com>
2022-02-26 23:29:05 +0800

    :tada:use gcd and fraction to hashcode as string

--- hash-table/149-直线上最多的点数.cpp
+++ hash-table/149-直线上最多的点数.cpp
@@ -15,34 +15,56 @@ using namespace std;
 class Solution
 {
 public:
-    string floatToString(float a)
+    // string floatToString(float a)
+    // {
+    //     ostringstream sss;
+    //     sss << setiosflags(ios::fixed) << setprecision(10) << a;
+    //     string s(sss.str());
+    //     return s;
+    // }
+    // int maxPoints(vector<vector<int>>& points)
+    // {
+    //     int                        result = 1;
+    //     unordered_map<string, int> kCount;
+    //     for (int i = 0; i < points.size(); i++) {
+    //         kCount.clear();
+    //         for (int j = i + 1; j < points.size(); j++) {
+    //             string k;
+    //             if (points[i][1] - points[j][1] == 0)
+    //                 k = "y=" + floatToString(points[i][1]);
+    //             else if (points[i][0] - points[j][0] == 0)
+    //                 k = "x=" + floatToString(points[i][0]);
+    //             else
+    //                 k = floatToString((float)(points[i][1] - points[j][1]) /
+    //                                   (points[i][0] - points[j][0]));
+    //             // printf("i:[%d,%d] j:[%d,%d] result:%d InALine:%d k:", points[i][0],
+    //             points[i][1],
+    //             //        points[j][0], points[j][1], result, kCount[k] + 2);
+    //             // for (auto m : k)
+    //             //     printf("%c", m);
+    //             // printf("\n");
+    //             result = max(result, ++kCount[k] + 1);
+    //         }
+    //     }
+    //     return result;
+    // }
+    int gcd(int a, int b)   //最大公约数
     {
-        ostringstream sss;
-        sss << setiosflags(ios::fixed) << setprecision(10) << a;
-        string s(sss.str());
-        return s;
+        return b == 0 ? a : gcd(b, a % b);
     }
     int maxPoints(vector<vector<int>>& points)
     {
-        int                        result = 1;
-        unordered_map<string, int> kCount;
+        if (points.size() == 0)
+            return 0;
+        int result = 1;
         for (int i = 0; i < points.size(); i++) {
-            kCount.clear();
+            unordered_map<string, int> kCount;
             for (int j = i + 1; j < points.size(); j++) {
-                string k;
-                if (points[i][1] - points[j][1] == 0)
-                    k = "y=" + floatToString(points[i][1]);
-                else if (points[i][0] - points[j][0] == 0)
-                    k = "x=" + floatToString(points[i][0]);
-                else
-                    k = floatToString((float)(points[i][1] - points[j][1]) /
-                                      (points[i][0] - points[j][0]));
-                // printf("i:[%d,%d] j:[%d,%d] result:%d InALine:%d k:", points[i][0], points[i][1],
-                //        points[j][0], points[j][1], result, kCount[k] + 2);
-                // for (auto m : k)
-                //     printf("%c", m);
-                // printf("\n");
-                result = max(result, ++kCount[k] + 1);
+                int    x      = points[i][0] - points[j][0];
+                int    y      = points[i][1] - points[j][1];
+                int    gcdNum = gcd(x, y);
+                string key    = to_string(x / gcdNum) + "_" + to_string(y / gcdNum);
+                result        = max(result, ++kCount[key] + 1);
             }
         }
         return result;
