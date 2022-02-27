a1303466d48e56d63c2b84db9199ec5c048781ee
N.O.Atomusk <orzkey@outlook.com>
2022-02-27 23:08:50 +0800

    :tada:220 use ordered set to find the number of correct range in a slide windows

--- hash-table/217-存在重复元素.cpp
+++ hash-table/217-存在重复元素.cpp
@@ -4,12 +4,27 @@
  * [217] 存在重复元素
  */
 
+#include <unordered_map>
+using namespace std;
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    bool containsDuplicate(vector<int>& nums) {
-
+    bool containsDuplicate(vector<int>& nums)
+    {
+        unordered_map<int, int> numIndex;
+        for (auto& i : nums) {
+            if (numIndex.find(i) != numIndex.end()) {
+                return true;
+            }
+            numIndex[i] = i;
+        }
+        return false;
     }
+    // bool containsDuplicate(vector<int>& nums)
+    // {
+    //     unordered_set numsSet(nums.begin(), nums.end());
+    //     return numsSet.size() == nums.size() ? false : true;
+    // }
 };
 // @lc code=end
-
--- ordered-map/220-存在重复元素 III.cpp
+++ ordered-map/220-存在重复元素 III.cpp
@@ -4,12 +4,34 @@
  * [220] 存在重复元素 III
  */
 
+#include <cstdio>
+#include <iterator>
+#include <set>
+#include <unordered_map>
+using namespace std;
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
-
+    bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t)
+    {
+        if (nums.size() < 2)
+            return false;
+        set<long> windowNum;
+        int       left = 0, right = 0;   //[left,right)
+        while (right < nums.size()) {
+            auto it = windowNum.lower_bound((long)nums[right] - t);
+            // printf("%d %d\n", nums[right], *it);
+            if (it != windowNum.end() && *it <= (long)nums[right] + t)
+                return true;
+            windowNum.insert((long)nums[right]);
+            right++;
+            while (right - 1 - left == k) {
+                windowNum.erase((long)nums[left]);
+                left++;
+            }
+        }
+        return false;
     }
 };
 // @lc code=end
-
