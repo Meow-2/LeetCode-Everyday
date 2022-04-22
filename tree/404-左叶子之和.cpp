1b621439e8f318b56f02821acda2d16863ed7bb0
N.O.Atomusk <orzkey@outlook.com>
2022-04-22 21:37:32 +0800

    :pencil:recursive out

--- tree/404-左叶子之和.cpp
+++ tree/404-左叶子之和.cpp
@@ -23,9 +23,7 @@ public:
     {
         if (!root)
             return 0;
-        if (!root->left)
-            return 0 + sumOfLeftLeaves(root->right);
-        if (!root->left->left && !root->left->right)
+        if (root->left && !root->left->left && !root->left->right)
             return root->left->val + sumOfLeftLeaves(root->right);
         return sumOfLeftLeaves(root->left) + sumOfLeftLeaves(root->right);
     }
