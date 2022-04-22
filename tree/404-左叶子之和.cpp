465cd01fbcdd695c9867118a0867e62278295246
N.O.Atomusk <orzkey@outlook.com>
2022-04-22 21:34:36 +0800

    :tada:recursive

---
+++ tree/404-左叶子之和.cpp
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=404 lang=cpp
+ *
+ * [404] 左叶子之和
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    int sumOfLeftLeaves(TreeNode* root)
+    {
+        if (!root)
+            return 0;
+        if (!root->left)
+            return 0 + sumOfLeftLeaves(root->right);
+        if (!root->left->left && !root->left->right)
+            return root->left->val + sumOfLeftLeaves(root->right);
+        return sumOfLeftLeaves(root->left) + sumOfLeftLeaves(root->right);
+    }
+};
+// @lc code=end
