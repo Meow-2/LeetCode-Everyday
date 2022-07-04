10a44173fc44b53bbeae6d2212219f1ae3e0f7f7
N.O.Atomusk <orzkey@outlook.com>
2022-07-04 18:50:25 +0800

    改进了三数之和的写法, 更加直观

--- array/15-三数之和.cpp
+++ array/15-三数之和.cpp
@@ -14,36 +14,35 @@ class Solution
 public:
     vector<vector<int>> threeSum(vector<int>& nums)
     {
-        if (nums.size() < 3)
-            return {};
-        int                 i = 0;
-        vector<vector<int>> result;
+        int                 i = 0, j = 0, k = 0;
+        vector<vector<int>> res;
+        //排序, 方便去重
         sort(nums.begin(), nums.end());
-        while (i < nums.size() - 2) {
-            if (nums[i] > 0)
-                break;
-            else {
-                int target = 0 - nums[i];
-                int left   = i + 1;
-                int right  = nums.size() - 1;
-                while (left < right) {
-                    if (nums[left] + nums[right] < target)
-                        left++;
-                    else if (nums[left] + nums[right] > target)
-                        right--;
-                    else {
-                        result.push_back({nums[i], nums[left], nums[right]});
-                        left++;
-                        while (left < nums.size() && nums[left] == nums[left - 1])
-                            left++;
-                    }
+        while (i < nums.size()) {
+            int target = 0 - nums[i];
+            int left   = i + 1;
+            int right  = nums.size() - 1;
+            // 对撞指针
+            while (left < right) {
+                if (nums[left] + nums[right] == target) {
+                    // 防止重复, 如果nums[left]前一个值和它一样, 那么说明已经统计过了
+                    // 当然如果 left = i + 1 的话, 那就说明还没有统计过
+                    if (nums[left] != nums[left - 1] || left == i + 1)
+                        res.push_back({nums[i], nums[left], nums[right]});
+                    left++;
+                    right--;
                 }
+                else if (nums[left] + nums[right] < target)
+                    left++;
+                else
+                    right--;
             }
-            i++;
-            while (i < nums.size() - 2 && nums[i] == nums[i - 1])
+            // i++; 如果 i++ 之后还是原来的 i ,则继续 i++, 直到不是原来的 i, 防止重复
+            do {
                 i++;
+            } while (i < nums.size() && nums[i] == nums[i - 1]);
         }
-        return result;
+        return res;
     }
 };
 // @lc code=end
