06f90ca6e04e64b44ad8ea4f791570a9782dbdb7
N.O.Atomusk <orzkey@outlook.com>
2022-07-06 12:52:53 +0800

    二刷,优先队列加lambda表达式

--- heap/23-合并K个升序链表.cpp
+++ heap/23-合并K个升序链表.cpp
@@ -48,35 +48,56 @@ public:
     //     }
     //     return head->next;
     // }
-    struct node
-    {
-        int       val;
-        ListNode* nodeAdress;
-        node(int a, ListNode* b) : val(a), nodeAdress(b)
-        {}
-        bool operator<(const node& a) const
-        {
-            return this->val > a.val;
-        }
-    };
+    // struct node
+    // {
+    //     int       val;
+    //     ListNode* nodeAdress;
+    //     node(int a, ListNode* b) : val(a), nodeAdress(b)
+    //     {}
+    //     bool operator<(const node& a) const
+    //     {
+    //         return this->val > a.val;
+    //     }
+    // };
 
+    // ListNode* mergeKLists(vector<ListNode*>& lists)
+    // {
+    //     auto                 head = new ListNode();
+    //     auto                 cur  = head;
+    //     priority_queue<node> q;
+    //     for (auto& i : lists)
+    //         if (i)
+    //             q.push(node(i->val, i));
+    //     while (!q.empty()) {
+    //         auto n = q.top();
+    //         q.pop();
+    //         cur->next = n.nodeAdress;
+    //         cur       = cur->next;
+    //         if (n.nodeAdress->next)
+    //             q.push(node(n.nodeAdress->next->val, n.nodeAdress->next));
+    //     }
+    //     return head->next;
+    // }
     ListNode* mergeKLists(vector<ListNode*>& lists)
     {
-        auto                 head = new ListNode();
-        auto                 cur  = head;
-        priority_queue<node> q;
-        for (auto& i : lists)
-            if (i)
-                q.push(node(i->val, i));
+        unique_ptr<ListNode> dummy_node = make_unique<ListNode>(0, nullptr);
+        auto                 cur_node   = dummy_node.get();
+        // 优先队列结合 lambda 表达式的用法
+        auto cmp = [](const ListNode* a, const ListNode* b) { return a->val > b->val; };
+        priority_queue<ListNode*, vector<ListNode*>, decltype(cmp)> q(cmp);
+        for (auto const& node : lists)
+            if (node)
+                q.push(node);
         while (!q.empty()) {
-            auto n = q.top();
+            auto min_node = q.top();
             q.pop();
-            cur->next = n.nodeAdress;
-            cur       = cur->next;
-            if (n.nodeAdress->next)
-                q.push(node(n.nodeAdress->next->val, n.nodeAdress->next));
+            cur_node->next = new ListNode(min_node->val, nullptr);
+            cur_node       = cur_node->next;
+            if (min_node->next) {
+                q.push(min_node->next);
+            }
         }
-        return head->next;
+        return dummy_node->next;
     }
 };
 // @lc code=end
