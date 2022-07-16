8be3d8b0047fc8d063583e3a85ec454eb8f23074
N.O.Atomusk <orzkey@outlook.com>
2022-07-16 17:36:15 +0800

    自己写的dp

--- dynamic-programming/53-最大子数组和.cpp
+++ dynamic-programming/53-最大子数组和.cpp
@@ -4,7 +4,7 @@
  * [53] 最大子数组和
  */
 
-#include <type_traits>
+#include <climits>
 #include <vector>
 using namespace std;
 // @lc code=start
@@ -13,15 +13,15 @@ class Solution
 public:
     int maxSubArray(vector<int>& nums)
     {
-        int sum = 0, smax = INT_MIN;
-        for (int num : nums) {
-            sum += num;
-            smax = max(smax, sum);
-            if (sum < 0) {
-                sum = 0;
-            }
+        // dp[i] 表示以下标i结尾的最大子序列和, 故只需要找出最大的dp[i]即可
+        vector<int> dp(nums.size(), INT_MIN);
+        dp[0]      = nums[0];
+        int result = dp[0];
+        for (int i = 1; i < nums.size(); ++i) {
+            dp[i]  = dp[i - 1] > 0 ? dp[i - 1] + nums[i] : nums[i];
+            result = max(dp[i], result);
         }
-        return smax;
+        return result;
     }
 };
 // @lc code=end
