6b6d04324afef6e050489014e08db59754673242
N.O.Atomusk <orzkey@outlook.com>
2022-07-21 16:38:44 +0800

    dfs超时, 使用dp, dp[i]表示s的前i个字符能否被字典表示

---
+++ dynamic-programming/139-单词拆分.cpp
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode.cn id=139 lang=cpp
+ *
+ * [139] 单词拆分
+ */
+#include <string>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+// @lc code=start
+class Solution
+{
+public:
+    // dfs 超时
+    // bool wordBreak(string s, vector<string>& wordDict)
+    // {
+    //     if (s.size() == 0)
+    //         return true;
+    //     for (auto& i : wordDict) {
+    //         int j;
+    //         for (j = 0; j < s.size() && j < i.size() && s[j] == i[j]; j++)
+    //             ;
+    //         if (j == i.size() && wordBreak(string(s.begin() + j, s.end()), wordDict))
+    //             return true;
+    //     }
+    //     return false;
+    // }
+    bool wordBreak(string s, vector<string>& wordDict)
+    {
+        unordered_set<string> se(wordDict.begin(), wordDict.end());
+        int                   n = s.size();
+        // dp[i]表示s的前i个字符能否被表示
+        vector<bool> dp(n + 1, false);
+        dp[0] = true;
+        for (int i = 1; i < n + 1; i++) {
+            dp[i] = false;
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && se.count(s.substr(j, i - j)))
+                    dp[i] = true;
+            }
+        }
+        return dp[n];
+    }
+};
+// @lc code=end
