1c438d16ba4c897103a0c47570a2cfecd47f39b7
N.O.Atomusk <orzkey@outlook.com>
2022-07-24 22:26:34 +0800

    使用数组替代哈希表

--- design/208-实现 Trie (前缀树).cpp
+++ design/208-实现 Trie (前缀树).cpp
@@ -3,48 +3,52 @@
  *
  * [208] 实现 Trie (前缀树)
  */
+#include <array>
 #include <memory>
 #include <string>
-#include <unordered_map>
-#include <unordered_set>
 using namespace std;
 // @lc code=start
 struct TrieNode
 {
-    unordered_map<char, unique_ptr<TrieNode>> child;
+    bool                            isWordEnd{false};
+    array<unique_ptr<TrieNode>, 26> child;
+    TrieNode(bool isWordEnd_ = false) : isWordEnd(isWordEnd_) {}
 };
 class Trie
 {
 public:
-    unordered_set<string> isExistString;
-    unique_ptr<TrieNode>  root;
+    unique_ptr<TrieNode> root;
     Trie() : root(make_unique<TrieNode>()) {}
 
     void insert(string const& word)
     {
-        // c++中一个引用指向的地址不会改变，改变的是指向地址的内容，然而java中引用指向的地址在变
-        // 老老实实用指针迭代而不要用引用迭代;
-        if (isExistString.count(word))
-            return;
-        isExistString.insert(word);
         auto ptr = root.get();
         for (auto ch : word) {
-            if (!ptr->child.count(ch))
-                ptr->child[ch] = make_unique<TrieNode>();
-            ptr = ptr->child[ch].get();
+            if (!ptr->child[ch - 'a'])
+                ptr->child[ch - 'a'] = make_unique<TrieNode>();
+            ptr = ptr->child[ch - 'a'].get();
         }
+        ptr->isWordEnd = true;
     }
 
-    bool search(string const& word) { return isExistString.count(word); }
+    bool search(string const& word)
+    {
+        auto ptr = root.get();
+        for (auto ch : word) {
+            if (!ptr->child[ch - 'a'])
+                return false;
+            ptr = ptr->child[ch - 'a'].get();
+        }
+        return ptr->isWordEnd ? true : false;
+    }
 
     bool startsWith(string const& prefix)
     {
-
         auto ptr = root.get();
         for (auto ch : prefix) {
-            if (!ptr->child.count(ch))
+            if (!ptr->child[ch - 'a'])
                 return false;
-            ptr = ptr->child[ch].get();
+            ptr = ptr->child[ch - 'a'].get();
         }
         return true;
     }
