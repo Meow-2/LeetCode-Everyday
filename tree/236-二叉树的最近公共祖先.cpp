57a549788cabb85548bb783efea5f7fbe8f5a0cd
N.O.Atomusk <orzkey@outlook.com>
2022-07-25 02:16:50 +0800

    添加递归解法

--- tree/236-二叉树的最近公共祖先.cpp
+++ tree/236-二叉树的最近公共祖先.cpp
@@ -28,44 +28,64 @@ class Solution
 {
 public:
     // 深拷贝
-    void backtrack(TreeNode* cur, TreeNode* p, vector<TreeNode*>& path, vector<TreeNode*>& p_path)
-    {
-        if (!cur)
-            return;
-        if (cur == p) {
-            // cout << path[0] << '\n';
-            p_path = path;
-            // cout << p_path[0] << '\n';
-            return;
-        }
-        // if (cur->left) {
-        path.push_back(cur->left);
-        backtrack(cur->left, p, path, p_path);
-        path.pop_back();
-        // }
+    // void backtrack(TreeNode* cur, TreeNode* p, vector<TreeNode*>& path, vector<TreeNode*>&
+    // p_path)
+    // {
+    //     if (!cur)
+    //         return;
+    //     if (cur == p) {
+    //         // cout << path[0] << '\n';
+    //         p_path = path;
+    //         // cout << p_path[0] << '\n';
+    //         return;
+    //     }
+    //     // if (cur->left) {
+    //     path.push_back(cur->left);
+    //     backtrack(cur->left, p, path, p_path);
+    //     path.pop_back();
+    //     // }
 
-        // if (cur->right) {
-        path.push_back(cur->right);
-        backtrack(cur->right, p, path, p_path);
-        path.pop_back();
-        // }
+    //     // if (cur->right) {
+    //     path.push_back(cur->right);
+    //     backtrack(cur->right, p, path, p_path);
+    //     path.pop_back();
+    //     // }
+    // }
+    // TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
+    // {
+    //     // cout << "root:" << root;
+    //     vector<TreeNode*> p_path;
+    //     vector<TreeNode*> q_path;
+    //     vector<TreeNode*> path = {root};
+    //     path.push_back(root);
+    //     // cout << "path_root:" << path[0] << '\n';
+    //     backtrack(root, p, path, p_path);
+    //     backtrack(root, q, path, q_path);
+    //     // cout << p_path << '\n';
+    //     // cout << q_path << '\n';
+    //     int i = 0;
+    //     for (i = 0; i < p_path.size() && i < q_path.size() && p_path[i] == q_path[i]; i++)
+    //         ;
+    //     return p_path[i - 1];
+    // }
+    TreeNode* res{nullptr};
+    bool      dfs(TreeNode* root, TreeNode* p, TreeNode* q)
+    {
+        if (!root)
+            return false;
+        bool flag = false;
+        if (root == q || root == p)
+            flag = true;
+        bool isLeftHas  = dfs(root->left, p, q);
+        bool isRightHas = dfs(root->right, p, q);
+        if (isLeftHas && isRightHas || (isLeftHas || isRightHas) && flag)
+            res = root;
+        return isLeftHas || isRightHas || flag;
     }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
     {
-        // cout << "root:" << root;
-        vector<TreeNode*> p_path;
-        vector<TreeNode*> q_path;
-        vector<TreeNode*> path = {root};
-        path.push_back(root);
-        // cout << "path_root:" << path[0] << '\n';
-        backtrack(root, p, path, p_path);
-        backtrack(root, q, path, q_path);
-        // cout << p_path << '\n';
-        // cout << q_path << '\n';
-        int i = 0;
-        for (i = 0; i < p_path.size() && i < q_path.size() && p_path[i] == q_path[i]; i++)
-            ;
-        return p_path[i - 1];
+        dfs(root, p, q);
+        return res;
     }
 };
 // @lc code=end
