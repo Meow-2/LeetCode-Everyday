f9c04512197c30c58f72f8fb7968b07aab9aca54
N.O.Atomusk <orzkey@outlook.com>
2023-01-21 10:57:57 +0800

    添加滚动数组的实现

--- Coding-Interviews/10-I-斐波那契数列.py
+++ Coding-Interviews/10-I-斐波那契数列.py
@@ -1,11 +1,25 @@
 class Solution:
     def fib(self, n: int) -> int:
-        MOD = 10 ** 9 + 7
-        if n < 2:
-            return n
-        p, q, r = 0, 0, 1
-        for i in range(2, n + 1):
-            p = q
-            q = r
-            r = (p + q) % MOD
-        return r
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        MOD = 10**9+7
+        dp = [0]*(n+1)
+        dp[0] = 0
+        dp[1] = 1
+        for i in range(2, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n] % MOD
+
+
+# class Solution:
+#     def fib(self, n: int) -> int:
+#         MOD = 10 ** 9 + 7
+#         if n < 2:
+#             return n
+#         a, b, c = 0, 0, 1
+#         for _ in range(2, n+1):
+#             a, b = b, c
+#             c = a + b
+#         return c % MOD
